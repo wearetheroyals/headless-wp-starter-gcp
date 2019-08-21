44238d39c8223f517ec0028182a05eaa363d4620
Jimmy Cann <mail@jimmycann.com>
2019-08-21 17:20:26 +1000

    Update backups

---
+++ wordpress/wp-content/themes/theroyals-headless-wp/blocks/block-vimeo-modal.php
@@ -0,0 +1,4 @@
+<div class="wp-block-vimeo-modal" data-video-placeholder="<? block_field( 'video-plc-img' ); ?>" data-video-name="<? block_field( 'video-name' ); ?>" data-video-id="<? block_field( 'video-id' ); ?>">
+  <p>Vimeo Video Modal: <? block_field( 'video-id' ) ?></p>
+  <img src="<? block_field( 'video-plc-img' ); ?>" alt="<? block_field( 'video-name' ); ?>" />
+</div>
