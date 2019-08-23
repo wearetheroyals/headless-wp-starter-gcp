643cb298b742477286a5dea301c7dfa47f9c855f
Jimmy Cann <mail@jimmycann.com>
2019-08-23 15:36:06 +1000

    Add custom editor styles to the theme

--- wordpress/wp-content/themes/theroyals-headless-wp/functions.php
+++ wordpress/wp-content/themes/theroyals-headless-wp/functions.php
@@ -31,30 +31,12 @@ require_once 'inc/acf-options.php';
 // Add GraphQL resolvers.
 require_once 'inc/graphql/resolvers.php';
 
-
-function display_twitter_element()
-{
-	?>
-    	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
-    <?php
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ */
+function theme_setup() {
+	// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'css/style-editor.css' );
 }
-
-function display_facebook_element()
-{
-	?>
-    	<input type="text" name="facebook_url" id="facebook_url" value="<?php echo get_option('facebook_url'); ?>" />
-    <?php
-}
-
-function display_theme_panel_fields()
-{
-	add_settings_section("section", "All Settings", null, "theme-options");
-
-	add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
-    add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
-
-    register_setting("section", "twitter_url");
-    register_setting("section", "facebook_url");
-}
-
-add_action("admin_init", "display_theme_panel_fields");
+add_action( 'after_setup_theme', 'theme_setup' );
