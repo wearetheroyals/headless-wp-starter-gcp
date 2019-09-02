617176cbca57fbdd367c0cb5e2e12083baab330a
Jimmy Cann <mail@jimmycann.com>
2019-09-02 13:03:31 +1000

    Get a rudimentary custom WP Settings API working with GQL

--- wordpress/wp-content/themes/theroyals-headless-wp/functions.php
+++ wordpress/wp-content/themes/theroyals-headless-wp/functions.php
@@ -40,3 +40,80 @@ function theme_setup() {
 		add_editor_style( 'css/style-editor.css' );
 }
 add_action( 'after_setup_theme', 'theme_setup' );
+
+// Add general global settings menu
+function display_site_author()
+{
+	$value = get_option( 'site_author' );
+	?>
+	<input type='text' name='site_author' value='<?php echo $value; ?>'>
+	<?php
+}
+
+function display_palette_color_0()
+{
+	$value = get_option( 'palette_color_0' );
+	?>
+	<input type='text' name='palette_color_0' value='<?php echo $value; ?>'>
+	<?php
+}
+
+
+function register_settings() {
+
+
+    $args = array(
+        'type' => 'string',
+        'sanitize_callback' => NULL,
+        // 'sanitize_callback' => 'sanitize_text_field',
+        'default' => NULL,
+        'show_in_graphql' => true,
+    );
+
+    register_setting("general", "site_author", $args);
+    register_setting("palette", "palette_color_0", $args);
+
+}
+
+function register_settings_groups() {
+
+    add_settings_section("general", "General", null, "theroyals_headless_wp");
+    add_settings_field("site_author", "Site Author", "display_site_author", "theroyals_headless_wp", "general");
+
+    add_settings_section("palette", "Palette", null, "theroyals_headless_wp");
+    add_settings_field("palette_color_0", "Palette Colour 0", "display_palette_color_0", "theroyals_headless_wp", "palette");
+
+}
+
+
+
+function theme_settings_page()
+{
+    ?>
+	    <div class="wrap">
+
+        <h1>The Royals Theme Headless WordPress - Settings</h1>
+
+	    <form method="post" action="options.php">
+
+            <?php
+	            settings_fields("general");
+	            settings_fields("palette");
+	            do_settings_sections("theroyals_headless_wp");
+	            submit_button();
+	        ?>
+	    </form>
+		</div>
+	<?php
+}
+
+
+function add_theme_options_item()
+{
+	// add_menu_page("project", "project", "manage_options", "theme-panel", "theme_settings_page", null, 99);
+	add_options_page( 'The Royals Headless Theme Settings', 'The Royals Headless Theme Settings', 'manage_options', 'the_royals_headless_theme_settings', 'theme_settings_page' );
+}
+
+add_action("admin_menu", "add_theme_options_item");
+add_action("init", "register_settings");
+add_action("admin_init", "register_settings_groups");
